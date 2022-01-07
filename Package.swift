4eeebff9135f697625888cfebbc6dafea6d9d69b
Ben Schultz <bens4lsu@gmail.com>
2022-01-07 14:29:46 -0600

    swift 5.5

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     name: "NachaPicker",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
-        .package(url: "https://github.com/vapor/leaf.git", .upToNextMinor(from: "3.0.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.3.0")),
+        .package(url: "https://github.com/vapor/leaf.git", .upToNextMajor(from: "3.0.0")),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "Leaf"]),
